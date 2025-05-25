7b3a4dcc39afa351623ee7e9d08651515f3eb93e
samuelnmu <samuelnmu@gmail.com>
2025-05-25 14:42:14 +0300

    restructured Finance Calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,14 +1,11 @@
-#Finance Calclator
-class financeCalculation:
-    income = float(input("Enter your monthly income: "))
-    expenses = float(input("Enter your total monthly expenses: "))
-    
-    monthlySavings = income - expenses
-    interestRate = 0.05
-    
-    #(Projected Savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05))
-    projectedSavings = monthlySavings * 12 + (monthlySavings * 12 * interestRate)
-    
-    print("Your monthly savings are: ", "$",monthlySavings)
-    print("Projected savings after one year, with interest, is: ", "$",projectedSavings)
-    
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+interest_rate = 0.05
+
+# Projected Savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * interest_rate)
+
+print("Your monthly savings are: $", monthly_savings)
+print("Projected savings after one year, with interest, is: $", projected_savings)
