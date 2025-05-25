69872e4d2150df5b69480834782d8982fa57e679
samuelnmu <samuelnmu@gmail.com>
2025-05-25 12:02:58 +0300

    Basic Operations Commit in python week 3

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,24 @@
+#This is the addition class of two variables
+class addition:
+    number1 = 10
+    number2 = 5
+    
+    answer = number1 + number2
+    print("Addition of", number1, "and ", number2, "=", answer)
+
+#This is the subtraction class of two variables   
+class subtraction:
+    number1 = 10
+    number2 = 5
+    
+    answer = number1 - number2
+    print("Subtraction 0f", number1, "and", number2, "=", answer)
+
+#This is the multiplication class of two variables
+class multiplication:
+    number1 = 10
+    number2 = 5
+    
+    answer = number1 * number2
+    print("Multiplication of", number1, "and", number2, "=", answer)
+    
