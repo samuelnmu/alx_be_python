9b9cfc7f61441c18fa8881e2c808dd620055163a
samuelnmu <samuelnmu@gmail.com>
2025-05-25 12:21:10 +0300

    Simple Interest catlculation commit week 3

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+class intrestRate:
+    #formula = (I = P * R * T)
+    r = 0.05 #intrest rate
+    p = 1000 #principal amout rep $1000
+    t = 3 #time rep 3 yrs
+    
+    intrest = (p * r * t)
+    print("The simple intrest is: ", intrest)
+
