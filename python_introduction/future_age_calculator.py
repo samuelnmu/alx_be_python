0a7e02301bfe80ee018be2a6123dae377e72859d
samuelnmu <samuelnmu@gmail.com>
2025-05-25 12:49:16 +0300

    Age calculator commit

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+#age calculator
+
+class ageCaluclator:
+    age = int(input("How old are you? "))
+    ageAdded = 27
+    
+    answer = age + ageAdded
+    print("In 2050, you will be ",answer, "years old.")
+    
